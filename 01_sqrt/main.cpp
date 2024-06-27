9528a55078950365e1185527ba35d8250d42e522
Sergey Shambir <sergey.shambir@ispring.ru>
2024-06-27 09:14:45 +0300

    Добавил реализацию функции Sqrt и первый каркас проекта

---
+++ 01_sqrt/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <utility>
+#include <cmath>
+#include <cstdio>
+
+double Sqrt(double x)
+{
+    double result = 1.0;
+    double epsilon = 0.1e-8 * std::max(x, 1.0);
+
+    while (true)
+    {
+        double square_miss = result * result - x;
+        double correction = square_miss / (2.0 * result);
+        result -= correction;
+
+        if (std::abs(correction) < epsilon)
+        {
+            return result;
+        }
+    }
+}
+
+int main()
+{
+    std::printf("%f\n", Sqrt(2.0));
+}
